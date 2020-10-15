6667351bee1e33156b3a768ed597481cbbbeb073
Majid Ghasemi <mjghcr@gmail.com>
2020-10-15 15:02:51 +0330

    Calculator functionality added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,46 @@
-document.querySelector('.wrapper--deleteAll').addEventListener('click', function () {
-    document.querySelector('.wrapper--result').innerHTML = '';
+let result = document.querySelector('.wrapper--result');
+let plusOperator = document.querySelector('.wrapper--plus');
+let timeOperator = document.querySelector('.wrapper--times');
+let minusOperator = document.querySelector('.wrapper--minus');
+let divideOperator = document.querySelector('.wrapper--divide');
+let equalOperator = document.querySelector('.wrapper--equal');
+let total = [];
+
+document
+  .querySelector('.wrapper--deleteAll')
+  .addEventListener('click', function () {
+      if (result.innerText) {
+          result.innerText = '';
+          total = []
+    }
+  });
+
+document
+  .querySelector('.wrapper--backspace')
+  .addEventListener('click', function () {
+    if (result.innerText) {
+      result.innerText = result.innerText.slice(0, result.innerText.length - 1);
+    }
+  });
+
+
+for (let i = 0; i < 10; i++) {
+  document
+    .querySelector(`#number--${i}`)
+      .addEventListener('click', function (el) {
+          let currentText = result.innerText;
+          result.innerHTML = currentText + el.target.value ;
+    });
+}
+
+plusOperator.addEventListener('click', function (el) {
+    let currentValue = result.innerText;
+    total.push(currentValue);
+    result.innerHTML = '';
+    result.innerHTML = el.target.value;
+    total.push(el.target.value);
+    // result.innerHTML = ''
+    return total;
 });
+
+console.log(total);
